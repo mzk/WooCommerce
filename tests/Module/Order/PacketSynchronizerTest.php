bab2261946612a927a281e74fcf94e33dfd8e00d
Oldřich Šálek <oldrich.salek@packeta.com>
2025-01-21 15:06:40 +0100

    GL-48: fix phpunit on php 8.3 (deprecated)

--- tests/Module/Order/PacketSynchronizerTest.php
+++ tests/Module/Order/PacketSynchronizerTest.php
@@ -22,6 +22,10 @@ class PacketSynchronizerTest extends TestCase {
 
 	private PacketSynchronizer $packetSynchronizer;
 	private Client|MockObject $client;
+	/**
+	 * @var (object&MockObject)|OptionsProvider|(OptionsProvider&object&MockObject)|(OptionsProvider&MockObject)|MockObject
+	 */
+	private MockObject|OptionsProvider $provider;
 
 	private function createPacketSynchronize(): void {
 		$this->client    = $this->createMock( Client::class );
